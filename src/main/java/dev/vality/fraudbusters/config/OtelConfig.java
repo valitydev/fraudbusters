b5dc09ff15b8aabca716d58302a33d1213fb5976
kostyastruga <koztyastruga@gmail.com>
2024-08-13 13:14:10 +0300

    Up woody

--- src/main/java/dev/vality/fraudbusters/config/OtelConfig.java
+++ src/main/java/dev/vality/fraudbusters/config/OtelConfig.java
@@ -1,6 +1,7 @@
 package dev.vality.fraudbusters.config;
 
 import dev.vality.fraudbusters.config.properties.OtelProperties;
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
@@ -9,9 +10,11 @@ import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class OtelConfig {
@@ -26,9 +29,28 @@ public class OtelConfig {
                         .build()).build())
                 .build();
 
-        return OpenTelemetrySdk.builder()
+        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
                 .setTracerProvider(sdkTracerProvider)
                 .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-                .buildAndRegisterGlobal();
+                .build();
+        registerGlobalOpenTelemetry(openTelemetrySdk);
+        return openTelemetrySdk;
+    }
+
+    private static void registerGlobalOpenTelemetry(OpenTelemetry openTelemetry) {
+        try {
+            GlobalOpenTelemetry.set(openTelemetry);
+        } catch (Exception e) {
+            log.warn("please initialize the ObservabilitySdk before starting the application");
+            // will throw an exception if it was already set - problem is that we cannot check if was set
+            // by a third-party library before
+            GlobalOpenTelemetry.resetForTest();
+            try {
+                GlobalOpenTelemetry.set(openTelemetry);
+            } catch (Exception ex) {
+                log.warn("unable to set GlobalOpenTelemetry", ex);
+                //now we give up - must be a race condition
+            }
+        }
     }
 }
