d8b41646fb63edabc80aca69c924916c781a685e
kostyastruga <koztyastruga@gmail.com>
2024-08-13 11:42:25 +0300

    Up woody

---
+++ src/main/java/dev/vality/fraudbusters/config/OtelConfig.java
@@ -0,0 +1,34 @@
+package dev.vality.fraudbusters.config;
+
+import dev.vality.fraudbusters.management.config.properties.OtelProperties;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class OtelConfig {
+
+    private final OtelProperties otelProperties;
+
+    @Bean
+    public OpenTelemetry openTelemetryConfig() {
+        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder()
+                        .setEndpoint(otelProperties.getResource())
+                        .build()).build())
+                .build();
+
+        return OpenTelemetrySdk.builder()
+                .setTracerProvider(sdkTracerProvider)
+                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                .buildAndRegisterGlobal();
+    }
+}
