dd9886d4bfa830541ec28b7d20e1704fd56ac12a
struga <kennny007@yandex.ru>
2024-08-19 17:00:32 +0300

    Up woody (#66)

--- src/main/java/dev/vality/fraudbusters/config/OtelConfig.java
+++ src/main/java/dev/vality/fraudbusters/config/OtelConfig.java
@@ -3,30 +3,42 @@ package dev.vality.fraudbusters.config;
 import dev.vality.fraudbusters.config.properties.OtelProperties;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class OtelConfig {
 
+    public static final String FRAUDBUSTERS = "fraudbusters";
     private final OtelProperties otelProperties;
 
     @Bean
     public OpenTelemetry openTelemetryConfig() {
+        Resource resource = Resource.getDefault()
+                .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, FRAUDBUSTERS)));
+
         SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-                .addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder()
-                        .setEndpoint(otelProperties.getResource())
-                        .build()).build())
+                .addSpanProcessor(BatchSpanProcessor.builder(OtlpHttpSpanExporter.builder()
+                                .setEndpoint(otelProperties.getResource())
+                                .setTimeout(Duration.ofMillis(otelProperties.getTimeout()))
+                                .build())
+                        .build())
+                .setResource(resource)
                 .build();
 
         OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
